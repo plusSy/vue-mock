20caf38f22772d0e52726c84e9c34e4400b23e0b
石一龙 <shiyilong@opg.cn>
2018-11-15 14:22:22 +0800

    提示

---
+++ src/pages/element/index.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+     <el-date-picker
+      v-model="filter.date"
+      type="date"
+      placeholder="选择日期"
+      :pickerOptions="pickerOptions">
+    </el-date-picker>
+    {{ filter.date }}
+
+    <hr>
+    <el-button @click="resetData">重置</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ElementUI',
+  data () {
+    return {
+      filter: {
+        date: null,
+        days: 1
+      },
+      pickerOptions: {
+        disabledDate: (date) => {
+          let curDate = new Date();
+          curDate.setTime(curDate.getTime() - 3600 * 1000 * 24 * this.filter.days);
+          let range = date.valueOf() < Date.parse(curDate)
+          return range
+        }
+      }
+    }
+  },
+  methods: {
+    /**
+     * 重置按钮 将原始值清空 并且 将限制日期 从昨天 变成了一周前
+     */
+    resetData () {
+      this.filter.date = '';
+      this.filter.days = 7;
+    }
+  }
+}
+</script>
+
--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 
 import Mock from '@/pages/mock/index'
+import Element from '@/pages/element/index'
 
 Vue.use(Router)
 
@@ -17,6 +18,11 @@ export default new Router({
       path: '/mock',
       name: 'Mock',
       component: Mock
+    },
+    {
+      path: '/ele',
+      name: 'Element',
+      component: Element
     }
   ]
 })
